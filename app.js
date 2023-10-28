e68709032f908879115dd6097162443d74bb915a
galeonn321 <santichiste@hotmail.com>
2023-10-28 06:43:15 +0200

    add new changes backend

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Connect to your MongoDB database
+mongoose.connect('mongodb://localhost/your-database', { useNewUrlParser: true });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Create a Mongoose schema and model
+const Schema = mongoose.Schema;
+const yourSchema = new Schema({ name: String });
+const YourModel = mongoose.model('YourModel', yourSchema);
+
+// Define Express routes that interact with the database
+app.get('/api/data', async (req, res) => {
+  try {
+    const data = await YourModel.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
