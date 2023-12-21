28530fb06d073d5a09f3d0eb950e347c7972e618
galeonn321 <santichiste@hotmail.com>
2023-12-21 23:35:52 +0100

    wow

---
+++ models/user.Model.ts
@@ -0,0 +1,26 @@
+// models/User.js
+const mongoose = require('mongoose');
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Create the User model
+const UserModel = mongoose.model('User', userSchema);
+
+// Export the User model
+module.exports = UserModel;
