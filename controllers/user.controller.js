9e8afab4ba98a87fd42511229508cd67a5f8cd16
galeonn321 <santichiste@hotmail.com>
2024-01-10 19:54:20 +0100

    small prettier change

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,97 +1,102 @@
 const userCtrl = {};
-const userModel = require('../models/user.Model');
-const bcrypt = require('bcrypt');
+const userModel = require("../models/user.Model");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 async function hashPassword(password) {
-    try {
-        const salt = await bcrypt.genSalt(saltRounds);
-        const hash = await bcrypt.hash(password, salt);
-        return hash;
-    } catch (error) {
-        throw error;
-    }
-};
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function decryptPassword(password, hashedPassword) {
-    try {
-        const decryptPass = await bcrypt.compare(password, hashedPassword);
-        return decryptPass;
-    } catch (error) {
-        // Handle errors appropriately (e.g., log, throw, or return a value)
-        res.status(500).json({
-            ok: false,
-            message: `Internal server error. ${error}`,
-            data: ""
-        });
-    }
-};
+  try {
+    const decryptPass = await bcrypt.compare(password, hashedPassword);
+    return decryptPass;
+  } catch (error) {
+    // Handle errors appropriately (e.g., log, throw, or return a value)
+    res.status(500).json({
+      ok: false,
+      message: `Internal server error. ${error}`,
+      data: "",
+    });
+  }
+}
 
 userCtrl.register = async (req, res) => {
-    try {
-        const userExists = await userModel.findOne({
-            $or: [{ username: req.body.username }, { email: req.body.email }],
-        });
+  try {
+    const userExists = await userModel.findOne({
+      $or: [{ username: req.body.username }, { email: req.body.email }],
+    });
 
-        if (userExists) {
-            return res.status(403).json({
-                ok: false,
-                message: 'The username or email already exists.',
-                data: ""
-            });
-        }
-
-        const newUser = new userModel({
-            username: req.body.username,
-            email: req.body.email,
-            password: await hashPassword(req.body.password),
-        });
-
-        await newUser.save();
-
-        res.status(200).json({
-            ok: true,
-            message: 'User registered successfully.',
-            data: newUser,
-        });
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            message: 'Internal server error.',
-            data: "",
-        });
+    if (userExists) {
+      return res.status(403).json({
+        ok: false,
+        message: "The username or email already exists.",
+        data: "",
+      });
     }
+
+    const newUser = new userModel({
+      username: req.body.username,
+      email: req.body.email,
+      password: await hashPassword(req.body.password),
+    });
+
+    await newUser.save();
+
+    res.status(200).json({
+      ok: true,
+      message: "User registered successfully.",
+      data: newUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: "Internal server error.",
+      data: "",
+    });
+  }
 };
 
 userCtrl.login = async (req, res) => {
-    try {
-        const user = await userModel.findOne({ $or: [{ username: req.body.username }, { email: req.body.email }] });
+  try {
+    const user = await userModel.findOne({
+      $or: [{ username: req.body.username }, { email: req.body.email }],
+    });
 
-        if (user) {
-            const matchPasswords = await decryptPassword(req.body.password, user.password);
+    if (user) {
+      const matchPasswords = await decryptPassword(
+        req.body.password,
+        user.password
+      );
 
-            if (matchPasswords) {
-                // Assuming you have a generateToken function
-                const token = generateToken(user);
+      if (matchPasswords) {
+        // Assuming you have a generateToken function
+        const token = generateToken(user);
 
-                return res.status(200).json({
-                    ok: true,
-                    message: `Welcome back ${user.username}`,
-                    data: { ...user._doc, password: null, token },
-                });
-            }
-        }
-
-        return res
-            .status(404)
-            .json({ ok: false, message: "No user found", data: "" });
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            message: error.message,
-            data: "",
+        return res.status(200).json({
+          ok: true,
+          message: `Welcome back ${user.username}`,
+          data: { ...user._doc, password: null, token },
         });
+      }
     }
+
+    return res
+      .status(404)
+      .json({ ok: false, message: "No user found", data: "" });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: error.message,
+      data: "",
+    });
+  }
 };
 
 module.exports = userCtrl;
